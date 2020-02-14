fd1d538a7bb104fa9256b2d1be1db598893dc351
Sam Decker <samdecker386@gmail.com>
2020-02-13 18:21:36 -0700

    Completed first two tasks

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,23 @@
            https://api.github.com/users/<your name>
 */
 
+const GITHUB_API = 'https://api.github.com';
+const GITHUB_USERNAME = 'samdecker386';
+
+const cards = document.querySelector('.cards');
+
+axios.get(GITHUB_API + '/users/' + GITHUB_USERNAME)
+   .then(function (response) {
+      // handle success
+      console.log(response);
+      cards.appendChild(createUserCard(response.data));
+
+   })
+   .catch(function (error) {
+      // handle error
+      console.log(error);
+   });
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,6 +62,71 @@ const followersArray = [];
 </div>
 
 */
+
+/**
+ * Accepts a user object that holds values pertaining to their GitHub profile
+ * @param {object} user 
+ */
+function createUserCard(user) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  //User Image from GitHub
+  const userImage = document.createElement('img');
+  userImage.src = user.avatar_url;
+
+  //Card Info & Components
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+
+  //User real name
+  const name = document.createElement('h3')
+  name.classList.add('name');
+  name.textContent = user.name;
+
+  //Username
+  const username = document.createElement('p')
+  username.classList.add('username')
+  username.textContent = user.login;
+
+  //User location
+  const location = document.createElement('p')
+  location.textContent = user.location;
+
+  //Profile link to github
+  const profile = document.createElement('p')
+  const profileLink = document.createElement('a');
+
+  profileLink.href = user.html_url;
+  profileLink.textContent = user.html_url;
+
+  profile.appendChild(profileLink);
+
+  //Follower counts
+  const followers = document.createElement('p')
+  followers.textContent = user.followers;
+  const following = document.createElement('p')
+  following.textContent = user.following;
+
+  //User bio
+  const bio = document.createElement('p')
+  bio.textContent = user.bio;
+
+  //Append all components of cardInfo
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  //Append user image and info to card
+  card.appendChild(userImage);
+  card.appendChild(cardInfo);
+
+  return card;
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
