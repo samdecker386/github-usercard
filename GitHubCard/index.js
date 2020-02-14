1d1fe790406aa7c9cb3674c4643541c168959e06
Sam Decker <samdecker386@gmail.com>
2020-02-14 00:03:12 -0700

    MVP and one stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,22 +3,17 @@
            https://api.github.com/users/<your name>
 */
 
-const GITHUB_API = 'https://api.github.com';
-const GITHUB_USERNAME = 'samdecker386';
-
 const cards = document.querySelector('.cards');
 
-axios.get(GITHUB_API + '/users/' + GITHUB_USERNAME)
-   .then(function (response) {
-      // handle success
-      console.log(response);
-      cards.appendChild(createUserCard(response.data));
-
-   })
-   .catch(function (error) {
-      // handle error
-      console.log(error);
-   });
+axios.get('https://api.github.com/users/samdecker386')
+  .then(response => {
+    console.log(response.data);
+    const card = gitCard(response.data);
+    cards.append(card);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -43,6 +38,20 @@ axios.get(GITHUB_API + '/users/' + GITHUB_USERNAME)
 
 const followersArray = [];
 
+axios.get('https://api.github.com/users/samdecker386/followers')
+.then(response => {
+  console.log(response.data);
+  response.data.forEach(item => {
+    followersArray.push(item.url);
+  });
+  followersArray.forEach(item => {
+    axios.get(item)
+    .then(response => {
+      cards.appendChild(gitCard(response.data));
+    });
+  });
+});
+
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -63,72 +72,55 @@ const followersArray = [];
 
 */
 
-/**
- * Accepts a user object that holds values pertaining to their GitHub profile
- * @param {object} user 
- */
-function createUserCard(user) {
+// create a function that will create a Git Hub user card that matches HTML
+function gitCard(user) {
+
+  // define new elements
   const card = document.createElement('div');
-  card.classList.add('card');
-
-  //User Image from GitHub
   const userImage = document.createElement('img');
-  userImage.src = user.avatar_url;
-
-  //Card Info & Components
   const cardInfo = document.createElement('div');
-  cardInfo.classList.add('card-info');
-
-  //User real name
-  const name = document.createElement('h3')
-  name.classList.add('name');
-  name.textContent = user.name;
-
-  //Username
-  const username = document.createElement('p')
-  username.classList.add('username')
-  username.textContent = user.login;
-
-  //User location
-  const location = document.createElement('p')
-  location.textContent = user.location;
-
-  //Profile link to github
-  const profile = document.createElement('p')
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
   const profileLink = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
 
-  profileLink.href = user.html_url;
-  profileLink.textContent = user.html_url;
-
-  profile.appendChild(profileLink);
-
-  //Follower counts
-  const followers = document.createElement('p')
-  followers.textContent = user.followers;
-  const following = document.createElement('p')
-  following.textContent = user.following;
-
-  //User bio
-  const bio = document.createElement('p')
-  bio.textContent = user.bio;
-
-  //Append all components of cardInfo
+  // setup the structure of the elements
+  card.appendChild(userImage);
+  card.appendChild(cardInfo);
   cardInfo.appendChild(name);
-  cardInfo.appendChild(username);
+  cardInfo.appendChild(userName);
   cardInfo.appendChild(location);
   cardInfo.appendChild(profile);
   cardInfo.appendChild(followers);
   cardInfo.appendChild(following);
   cardInfo.appendChild(bio);
+  profile.appendChild(profileLink);
 
-  //Append user image and info to card
-  card.appendChild(userImage);
-  card.appendChild(cardInfo);
+  // add classes to elements
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+  // set src, and href, text content
+  userImage.src = user.avatar_url;
+  name.textContent = user.name;
+  userName.textContent = user.login;
+  location.textContent = user.location;
+  profileLink.textContent = user.html_url;
+  profileLink.href = user.html_url;
+  followers.textContent = user.followers;
+  following.textContent = user.following;
+  bio.textContent = user.bio;
 
   return card;
-}
+};
 
-/* List of LS Instructors Github username's: 
+/* List of LS Instructors Github username's:
   tetondan
   dustinmyers
   justsml
